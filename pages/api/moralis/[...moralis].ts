933acc48d6a57b14b7e1977b2744f5a259222e96
guschi18 <philipp.guschl@gmx.de>
2023-05-06 23:08:29 +0200

    uri set

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -7,7 +7,7 @@ export default MoralisNextApi({
     apiKey: 'FR1JikkPoWKsk5bn3YNxHRV1Xn1SQBabvJESOLPgPUXWxJd3fgksjkrNLDXSAhGL',
     authentication: {
     domain: "amazing.dapp",
-    uri: 'http://localhost:3000',
+    uri: 'https://web3-navigator-io.vercel.app',
     timeout: 120,
   },
 });
